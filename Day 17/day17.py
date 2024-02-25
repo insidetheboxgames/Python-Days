360517e2970cce44521d0ceec03e22ede78b3b3f
Aidan Covey <70307451+insidetheboxgames@users.noreply.github.com>
2024-02-24 19:03:35 -0800

    Day 17

---
+++ Day 17/day17.py
@@ -0,0 +1,7 @@
+# day 17 - 30 day challenge: Exception Handling
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries,es,rs = names
+print(nordic_countries)
+print(es)
+print(rs)
